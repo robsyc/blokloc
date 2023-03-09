00c33032e719dba7c7fe497e6d922e8e3adbaeed
robsyc <robbe.claeys@gmail.com>
2023-03-09 14:04:31 +0100

    updated schema

--- blokloc.sql
+++ blokloc.sql
@@ -2,46 +2,57 @@ DROP database bldb;
 CREATE database bldb;
 USE bldb;
 
-CREATE TABLE `locations` (
-  `id` INT PRIMARY KEY,
-  `title` VARCHAR(128),
-  `label` VARCHAR(128),
-  `address` VARCHAR(255),
-  `tot_cap` INT UNSIGNED,
-  `tot_res` INT UNSIGNED,
-  `t_open` TIME,
-  `t_close` TIME,
-  `latitude` DECIMAL(18,16),
-  `longitude` DECIMAL(18,16),
-  `teaser_img_url` VARCHAR(255),
-  `more_info_url` VARCHAR(255),
-  `input_general` FLOAT(5) CHECK (input_general >= 0 AND input_general <= 5),
-  `input_occupancy` FLOAT(5) CHECK (input_occupancy >= 0 AND input_occupancy <= 3),
-  `input_internet` FLOAT(5) CHECK (input_internet >= 0 AND input_internet <= 3),
-  `input_electr` FLOAT(5) CHECK (input_electr >= 0 AND input_electr <= 3)
-  );
-
-CREATE TABLE `users` (
-  `id` INT PRIMARY KEY,
-  `name` VARCHAR(255),
-  `ncomments` INT UNSIGNED,
-  `input_score` FLOAT(5) DEFAULT 0.5 CHECK (input_score >= 0 AND input_score <= 1),
-  `avg_rating` FLOAT(5) CHECK (avg_rating >= 0 AND avg_rating <= 5),
-  `avg_comment_rating` FLOAT(5) CHECK (avg_comment_rating >= 0 AND avg_comment_rating <= 5)
+CREATE TABLE `location` (
+	`id` INT PRIMARY KEY,
+	`title` VARCHAR(128),
+	`label` VARCHAR(128),
+	`address` VARCHAR(255),
+	`tot_cap` INT UNSIGNED,
+	`tot_res` INT UNSIGNED,
+	`latitude` DECIMAL(18,16),
+	`longitude` DECIMAL(18,16),
+	`teaser_img_url` VARCHAR(255),
+	`more_info_url` VARCHAR(255),
+	`t_open` TIME,
+	`t_close` TIME,
+	`avg_rating` FLOAT(5) CHECK (avg_rating >= 0 AND avg_rating <= 5),
+	`avg_occupancy` FLOAT(5) CHECK (avg_occupancy >= 0 AND avg_occupancy <= 5),
+	`avg_internet` FLOAT(5) CHECK (avg_internet >= 0 AND avg_internet <= 5)
 );
 
-CREATE TABLE `reviews` (
-  `id` INT PRIMARY KEY,
-  `user_id` INT,
-  `loc_id` INT,
-  `date` DATETIME,
-  `input_text` TEXT,
-  `input_general` ENUM("x-sad", "sad", "neutral", "happy", "x-happy"),
-  `input_occupancy` ENUM("sad", "neutral", "happy"),
-  `input_internet` ENUM("sad", "neutral", "happy"),
-  `input_electr` ENUM("sad", "neutral", "happy"),
-  `comment_rating` FLOAT(5) CHECK (comment_rating >= 0 AND comment_rating <= 5)
+CREATE TABLE `user` (
+	`id` INT PRIMARY KEY,
+	`name` VARCHAR(255),
+	`ncomments` INT UNSIGNED,
+	`avg_rating` FLOAT(5) CHECK (avg_rating >= 0 AND avg_rating <= 5),
+	`avg_rated` FLOAT(5) CHECK (avg_rated >= 0 AND avg_rated <= 5)
 );
 
-ALTER TABLE `reviews` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-ALTER TABLE `reviews` ADD FOREIGN KEY (`loc_id`) REFERENCES `locations` (`id`);
+CREATE TABLE `review` (
+	`id` INT PRIMARY KEY,
+	`user_id` INT,
+	`loc_id` INT,
+	`date` DATETIME,
+	`avg_rating` FLOAT(5) CHECK (avg_rating >= 0 AND avg_rating <= 5)
+);
+
+CREATE TABLE `observation` (
+	`id` INT PRIMARY KEY,
+	`review_id` INT,
+    `attribute` ENUM("obs_t_open", "obs_t_close", "obs_text", "obs_rating", "obs_occupancy", "obs_internet"),
+    `input` VARCHAR(255)
+);
+
+CREATE TABLE `rating` (
+	`id` INT PRIMARY KEY,
+    `user_id` INT,
+    `review_id` INT,
+    `input` VARCHAR(255)
+);
+
+ALTER TABLE `review` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `review` ADD FOREIGN KEY (`loc_id`) REFERENCES `location` (`id`);
+ALTER TABLE `observation` ADD FOREIGN KEY (`review_id`) REFERENCES `review` (`id`);
+ALTER TABLE `rating` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+ALTER TABLE `rating` ADD FOREIGN KEY (`review_id`) REFERENCES `review` (`id`);
+CREATE INDEX `obs_attribute` ON observation (attribute);
